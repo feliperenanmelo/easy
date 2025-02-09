edcd741598041d1d6bc701e32b9be198737ebae2
Felipe Melo <feliperenanmelo1988@gmail.com>
2025-02-08 21:42:04 -0300

    feat: desafio dev easy

---
+++ EasyTest/Program.cs
@@ -0,0 +1,38 @@
+﻿static bool WordIsUpperCase(string word)
+{
+    var wordLenght = word.Length;
+
+    if(wordLenght < 1 || wordLenght > 100) return false;
+
+    bool isUpperCase = false;
+    bool isLowerCase = false;
+
+    var countUppercaseLetters = 0;
+
+    foreach (char letter in word)
+    {
+        if(char.IsUpper(letter))
+        {
+            isUpperCase = true;
+
+            countUppercaseLetters += 1;
+
+            if (isLowerCase) return false;
+
+            continue;
+        }
+
+        if (char.IsLower(letter))
+        {
+            if (countUppercaseLetters > 1 && isUpperCase) return false;
+
+            isLowerCase = true;
+            
+            continue;
+        }
+
+        return false;
+    }
+
+    return isUpperCase || isLowerCase;
+}
